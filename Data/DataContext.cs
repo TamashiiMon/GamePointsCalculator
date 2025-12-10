634bb6331326548819251de47371432be296015c
TamashiiMon <tamashiimon@gmail.com>
2025-12-10 11:22:58 +0100

    Cooler comit

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,4 +4,45 @@ public class DataContext
 {
     public static List<Player> Players = new();
 
+    public static byte[] ExportCSV()
+    {
+        using var memoryStream = new MemoryStream();
+        using (var writer = new StreamWriter(memoryStream, leaveOpen: true))
+        {
+            writer.WriteLine($"{DataType.PRENAME},{DataType.LASTNAME},{DataType.POINTS},{DataType.USERID}");
+
+            foreach (var player in Players)
+            {
+                writer.WriteLine(player.ToCSV());
+            }
+            writer.Flush();
+        }
+        return memoryStream.ToArray();
+    }
+
+    
+    public static void ImportCSV(string[] lines)
+    {
+
+        Dictionary<DataType, int> DataTypeMap = new();
+        Players.Clear();
+        foreach (var line in lines)
+        {
+            if (line.EndsWith("USERID"))
+            {
+                string[] columns = line.Split(',');
+                int columnsCount = 0;
+                foreach (var column in columns)
+                {
+                    if (column == "USERID") DataTypeMap.TryAdd(DataType.USERID, columnsCount);
+                    else if (column == "PRENAME") DataTypeMap.TryAdd(DataType.PRENAME, columnsCount);
+                    else if (column == "LASTNAME") DataTypeMap.TryAdd(DataType.LASTNAME, columnsCount);
+                    else if (column == "POINTS") DataTypeMap.TryAdd(DataType.POINTS, columnsCount);
+                    columnsCount++;
+                }
+                continue;
+            }
+            Players.Add(Player.FromCSV(line, DataTypeMap));
+        }
+    }
 }
--- Data/Player.cs
+++ Data/Player.cs
@@ -1,24 +1,45 @@
-namespace Data;
+using System.ComponentModel.DataAnnotations;
+using Data;
+using DataType = Data.DataType;
 
 public class Player
 {
     public int UserId { get; set; }
     
+    [Required(ErrorMessage = "First name is required")]
     public string PreName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required")]
     public string LastName { get; set; }
+
+    [Range(0, 999, ErrorMessage = "Points must be between 0 and 999")]
     public int Points { get; set; }
 
     public Player()
     {
         UserId = DataContext.Players.Count + 1;
     }
-
-    public bool isVailed()
+    
+    public string ToCSV()
     {
-        if (UserId != 0 && PreName != "" && LastName != "")
-        {
-            return true;
-        } 
-        return false;
+        return $"{this.PreName},{this.LastName},{this.Points},{this.UserId}";
+    }
+
+
+    public static Player FromCSV(string csv, Dictionary<DataType, int> types)
+    {
+        var values = csv.Split(",");
+        
+        int preNameIndex = types.TryGetValue(DataType.PRENAME, out preNameIndex) ? preNameIndex : 0;
+        int lastNameIndex = types.TryGetValue(DataType.LASTNAME,  out lastNameIndex) ? lastNameIndex : 0;
+        int pointsIndex = types.TryGetValue(DataType.POINTS, out pointsIndex) ? pointsIndex : 0;
+        int userIdIndex = types.TryGetValue(DataType.USERID, out userIdIndex ) ? userIdIndex : 0;
+
+        var player = new Player();
+        player.PreName = values[preNameIndex];
+        player.LastName = values[lastNameIndex];
+        player.Points = int.Parse(values[pointsIndex]);
+        player.UserId = int.Parse(values[userIdIndex]);
+        return player;
     }
 }
