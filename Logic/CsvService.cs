630df35f9671024fbef80c306088ea032ce3ab3f
TamashiiMon <tamashiimon@gmail.com>
2025-12-10 13:42:14 +0100

    Fix thinks and put ctv functions from Util to CSV Service.

--- Logic/CsvService.cs
+++ Logic/CsvService.cs
@@ -1,4 +1,6 @@
 using Data;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
 
 namespace Logic;
 
@@ -44,5 +46,37 @@ public class CsvService
             }
             DataContext.Players.Add(Player.FromCSV(line, DataTypeMap));
         }
+    }
+    
+    public static async Task DownloadCSV(IJSRuntime JS)
+    {
+        var bytes = CsvService.ExportCSV();
+        var base64 = Convert.ToBase64String(bytes);
+
+        string js = $@"
+            const link = document.createElement('a');
+            link.download = 'players.csv';
+            link.href = 'data:text/csv;base64,{base64}';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        ";
+
+        await JS.InvokeVoidAsync("eval", js);
+    }
+
+    public static async Task UploadCSV(IBrowserFile file)
+    {
+        var buffer = new byte[file.Size];
+
+        using (var stream = file.OpenReadStream(10 * 1024 * 1024))
+        {
+            await stream.ReadAsync(buffer, 0, (int)file.Size);
+        }
+
+        var content = System.Text.Encoding.UTF8.GetString(buffer);
+        var lines = content.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+        CsvService.ImportCSV(lines);
     }
 }
--- Logic/UtilService.cs
+++ Logic/UtilService.cs
@@ -1,6 +1,4 @@
 using Data;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.JSInterop;
 
 namespace Logic;
 
@@ -16,42 +14,10 @@ public class UtilService
        
         foreach (var highestMember in highestMembers)
         {
-            if (string.IsNullOrEmpty(winnerText)) winnerText = highestMember.ToString();
-            else  winnerText += " & " + highestMember;
+            if (string.IsNullOrEmpty(winnerText)) winnerText = highestMember.PreName;
+            else  winnerText += " & " + highestMember.PreName;
         }
         
         return "Winner is: " + winnerText;
-    }
-    
-    public static async Task DownloadCSV(IJSRuntime JS)
-    {
-        var bytes = CsvService.ExportCSV();
-        var base64 = Convert.ToBase64String(bytes);
-
-        string js = $@"
-            const link = document.createElement('a');
-            link.download = 'players.csv';
-            link.href = 'data:text/csv;base64,{base64}';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        ";
-
-        await JS.InvokeVoidAsync("eval", js);
-    }
-
-    public static async Task UploadCSV(IBrowserFile file)
-    {
-        var buffer = new byte[file.Size];
-
-        using (var stream = file.OpenReadStream(10 * 1024 * 1024))
-        {
-            await stream.ReadAsync(buffer, 0, (int)file.Size);
-        }
-
-        var content = System.Text.Encoding.UTF8.GetString(buffer);
-        var lines = content.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-        CsvService.ImportCSV(lines);
     }
 }
